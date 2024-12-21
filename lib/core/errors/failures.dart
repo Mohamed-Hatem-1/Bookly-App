01836e6ffb6ca4df20e0abe073ffc7dc267963e4
Mohamed Hatem <mo.hatem1911@gmail.com>
2024-12-21 21:43:55 +0200

    implement error handling for API requests; add ApiService and HomeRepo implementation; update dependencies

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,13 +1,61 @@
-abstract class Failure {
-    final String message;
+import 'package:dio/dio.dart';
 
-    Failure(this.message);
+abstract class Failure {
+  final String message;
+  Failure(this.message);
 }
 
 class ServerFailure extends Failure {
-    ServerFailure(String message) : super(message);
+  ServerFailure(super.message);
+  factory ServerFailure.fromDioException(DioException e) {
+    if (e.type == DioExceptionType.connectionTimeout) {
+      return ServerFailure('Connection timeout');
+    }
+    if (e.type == DioExceptionType.sendTimeout) {
+      return ServerFailure('Send timeout');
+    }
+    if (e.type == DioExceptionType.receiveTimeout) {
+      return ServerFailure('Receive timeout');
+    }
+    if (e.type == DioExceptionType.badCertificate) {
+      return ServerFailure('Bad certificate');
+    }
+    if (e.type == DioExceptionType.badResponse) {
+      return ServerFailure.fromResponse(
+          e.response!.data, e.response!.statusCode!);
+    }
+    if (e.type == DioExceptionType.cancel) {
+      return ServerFailure('Request cancelled');
+    }
+    if (e.type == DioExceptionType.connectionError) {
+      return ServerFailure('Connection error');
+    }
+    if (e.type == DioExceptionType.unknown) {
+      return ServerFailure('Unknown error, please try again later');
+    }
+    return ServerFailure('Unknown error, please try again later');
+  }
+
+  factory ServerFailure.fromResponse(dynamic response, int statusCode) {
+    if (statusCode == 400) {
+      return ServerFailure('Bad request');
+    }
+    if (statusCode == 401) {
+      return ServerFailure('Unauthorized');
+    }
+    if (statusCode == 403) {
+      return ServerFailure('Forbidden');
+    }
+    if (statusCode == 404) {
+      return ServerFailure('Not found');
+    }
+    if (statusCode == 500) {
+      return ServerFailure('Internal server error, please try again later');
+    }
+    return ServerFailure(response['error']['message']);
+  }
 }
 
 class CacheFailure extends Failure {
-    CacheFailure(String message) : super(message);
+  CacheFailure(super.message);
 }
